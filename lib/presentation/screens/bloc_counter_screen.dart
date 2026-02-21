fbfa00fde00bc346ad4af7c0c8d9e54e9b54b932
Gabriel <gabrielbaez023@gmail.com>
2026-02-21 09:38:40 -0300

    Utilizando counter bloc

--- lib/presentation/screens/bloc_counter_screen.dart
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_bloc/counter_bloc.dart';
 
 class BlocCounterScreen extends StatelessWidget {
   const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterBloc(),
+      child: BlocCounterView()
+    );
+  }
+}
+
+void increaseCounterBy(BuildContext context, [int value = 1]){
+  context.read<CounterBloc>()
+  .add(CounterIncreased(value));
+}
+
+class BlocCounterView extends StatelessWidget {
+  const BlocCounterView({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +37,9 @@ class BlocCounterScreen extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: const Text('Counter Value: 0000'),
+        child: context.select(
+          (CounterBloc counterBloc) => Text('Counter Value: ${counterBloc.state.counter}')
+        ) ,
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -24,23 +47,23 @@ class BlocCounterScreen extends StatelessWidget {
           FloatingActionButton(
             heroTag: '1',
             child: const Text('+3'),
-            onPressed: () => {},
+            onPressed: () => increaseCounterBy(context, 3),
           ),
           
           const SizedBox(height: 15,),
-
+    
            FloatingActionButton(
             heroTag: '2',
             child: const Text('+2'),
-            onPressed: () => {},
+            onPressed: () => increaseCounterBy(context, 2),
           ),
-
+    
           const SizedBox(height: 15,),
-
+    
            FloatingActionButton(
             heroTag: '3',
             child: const Text('+1'),
-            onPressed: () => {},
+            onPressed: () => increaseCounterBy(context, 1),
           )
         ],
       ),
